964b15b3f5ba0117fe758f0fc5fb223abc1e809c
daose <bychen@uwaterloo.ca>
2016-08-25 22:57:10 -0400

    much faster loading by blocking images/ads

--- app/src/main/java/com/daose/anime/web/CustomWebClient.java
+++ app/src/main/java/com/daose/anime/web/CustomWebClient.java
@@ -5,11 +5,19 @@ import android.webkit.WebResourceResponse;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import java.io.ByteArrayInputStream;
+import java.util.HashSet;
+
 public class CustomWebClient extends WebViewClient {
     private static final String TAG = CustomWebClient.class.getSimpleName();
+    private static final WebResourceResponse dud = new WebResourceResponse("text/plain", "utf-8", new ByteArrayInputStream("".getBytes()));
+
+    private static HashSet<String> ignoreUrls;
+    private static String[] ignoreKeys = {"/images/", ".png", ".css", ".jpeg", ".jpg", "/ads/", "disqus", "facebook"};
 
     public CustomWebClient() {
         super();
+        ignoreUrls = new HashSet<String>();
     }
 
     @Override
@@ -25,10 +33,28 @@ public class CustomWebClient extends WebViewClient {
     }
 
     @Override
-    public WebResourceResponse shouldInterceptRequest(WebView view, String url){
-        //TODO:: block ads here: http://www.hidroh.com/2016/05/19/hacking-up-ad-blocker-android/
-        Log.d(TAG, "shouldInterceptRequest: " + url);
-        return null;
+    public WebResourceResponse shouldInterceptRequest(WebView view, String url) {
+        if (ignoreUrls.contains(url)) {
+            return dud;
+        }
+
+        //cloudflare, pass
+        if (url.contains("answer")) return null;
+
+        if (url.contains("kissanime")) {
+            for (String key : ignoreKeys) {
+                if (url.contains(key)) {
+                    Log.d(TAG, "IGNORE: " + url);
+                    ignoreUrls.add(url);
+                    return dud;
+                }
+            }
+            Log.d(TAG, "PASS: " + url);
+            return null;
+        } else {
+            ignoreUrls.add(url);
+            return dud;
+        }
     }
 
     @Override
