12db6f025a2ab6701435de3fbdb9fc3661ab74a5
daose <bychen@uwaterloo.ca>
2017-03-18 09:53:42 -0400

    check 3 times every 24 hours

--- app/src/main/java/com/daose/ksanime/MyApplication.java
+++ app/src/main/java/com/daose/ksanime/MyApplication.java
@@ -7,14 +7,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.SystemClock;
-import android.util.Log;
 
 import com.daose.ksanime.service.KAService;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
 
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
@@ -48,7 +42,7 @@ public class MyApplication extends Application {
         alarmManager.setRepeating(
                 AlarmManager.ELAPSED_REALTIME_WAKEUP,
                 SystemClock.elapsedRealtime(),
-                AlarmManager.INTERVAL_HALF_DAY,
+                AlarmManager.INTERVAL_DAY / 3,
                 pendingIntent
         );
     }
